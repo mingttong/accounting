faea23484f1db0afc74a4a34e70218b140977099
zhouwunan <zhouwunan@58ganji.com>
2018-08-13 23:06:41 +0800

    评论

--- src/pages/mock.js
+++ src/pages/mock.js
@@ -74,4 +74,11 @@ module.exports = {
             'https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1534175502731&di=2b2fd4b6300d9f000d6dccbe6d3ad8c5&imgtype=0&src=http%3A%2F%2Fatt.bbs.duowan.com%2Fforum%2F201407%2F26%2F211802oixbk4pppzogpbiy.jpg',
         ],
     },
+
+    comment: {
+        images: [
+            'https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1534175502731&di=2b2fd4b6300d9f000d6dccbe6d3ad8c5&imgtype=0&src=http%3A%2F%2Fatt.bbs.duowan.com%2Fforum%2F201407%2F26%2F211802oixbk4pppzogpbiy.jpg',
+            'https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1534175502731&di=2b2fd4b6300d9f000d6dccbe6d3ad8c5&imgtype=0&src=http%3A%2F%2Fatt.bbs.duowan.com%2Fforum%2F201407%2F26%2F211802oixbk4pppzogpbiy.jpg',
+        ],
+    },
 };
