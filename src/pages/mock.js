e4c582ff56feef99cabcf57e3117b288f3bf3758
zhouwunan <zhouwunan@58ganji.com>
2018-08-13 21:45:32 +0800

    帮该死的信天翁项目做小程序页面

---
+++ src/pages/mock.js
@@ -0,0 +1,77 @@
+module.exports = {
+    recommand: [
+        {
+            name: 'Jay',
+            avator: 'http://pic7.58cdn.com.cn/anjuke_58/4b71f584c031a1267559ede388104bc5?w=62',
+            tags: [
+                '技术好',
+                '人好',
+            ],
+            album: [
+                'http://pic7.58cdn.com.cn/anjuke_58/4b71f584c031a1267559ede388104bc5?w=62',
+                'http://pic7.58cdn.com.cn/anjuke_58/4b71f584c031a1267559ede388104bc5?w=62',
+                'http://pic7.58cdn.com.cn/anjuke_58/4b71f584c031a1267559ede388104bc5?w=62',
+            ],
+        },
+        {
+            name: 'Jay',
+            avator: 'http://pic7.58cdn.com.cn/anjuke_58/4b71f584c031a1267559ede388104bc5?w=62',
+            tags: [
+                '技术好',
+                '人好',
+            ],
+            album: [
+                'http://pic7.58cdn.com.cn/anjuke_58/4b71f584c031a1267559ede388104bc5?w=62',
+                'http://pic7.58cdn.com.cn/anjuke_58/4b71f584c031a1267559ede388104bc5?w=62',
+                'http://pic7.58cdn.com.cn/anjuke_58/4b71f584c031a1267559ede388104bc5?w=62',
+            ],
+        },
+        {
+            name: 'Jay',
+            avator: 'http://pic7.58cdn.com.cn/anjuke_58/4b71f584c031a1267559ede388104bc5?w=62',
+            tags: [
+                '技术好',
+                '人好',
+            ],
+            album: [
+                'http://pic7.58cdn.com.cn/anjuke_58/4b71f584c031a1267559ede388104bc5?w=62',
+                'http://pic7.58cdn.com.cn/anjuke_58/4b71f584c031a1267559ede388104bc5?w=62',
+                'http://pic7.58cdn.com.cn/anjuke_58/4b71f584c031a1267559ede388104bc5?w=62',
+            ],
+        },
+        {
+            name: 'Jay',
+            avator: 'http://pic7.58cdn.com.cn/anjuke_58/4b71f584c031a1267559ede388104bc5?w=62',
+            tags: [
+                '技术好',
+                '人好',
+            ],
+            album: [
+                'http://pic7.58cdn.com.cn/anjuke_58/4b71f584c031a1267559ede388104bc5?w=62',
+                'http://pic7.58cdn.com.cn/anjuke_58/4b71f584c031a1267559ede388104bc5?w=62',
+                'http://pic7.58cdn.com.cn/anjuke_58/4b71f584c031a1267559ede388104bc5?w=62',
+            ],
+        },
+        {
+            name: 'Jay',
+            avator: 'http://pic7.58cdn.com.cn/anjuke_58/4b71f584c031a1267559ede388104bc5?w=62',
+            tags: [
+                '技术好',
+                '人好',
+            ],
+            album: [
+                'http://pic7.58cdn.com.cn/anjuke_58/4b71f584c031a1267559ede388104bc5?w=62',
+                'http://pic7.58cdn.com.cn/anjuke_58/4b71f584c031a1267559ede388104bc5?w=62',
+                'http://pic7.58cdn.com.cn/anjuke_58/4b71f584c031a1267559ede388104bc5?w=62',
+            ],
+        },
+    ],
+
+    homePage: {
+        videos: [
+            'https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1534175502731&di=2b2fd4b6300d9f000d6dccbe6d3ad8c5&imgtype=0&src=http%3A%2F%2Fatt.bbs.duowan.com%2Fforum%2F201407%2F26%2F211802oixbk4pppzogpbiy.jpg',
+            'https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1534175502731&di=2b2fd4b6300d9f000d6dccbe6d3ad8c5&imgtype=0&src=http%3A%2F%2Fatt.bbs.duowan.com%2Fforum%2F201407%2F26%2F211802oixbk4pppzogpbiy.jpg',
+            'https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1534175502731&di=2b2fd4b6300d9f000d6dccbe6d3ad8c5&imgtype=0&src=http%3A%2F%2Fatt.bbs.duowan.com%2Fforum%2F201407%2F26%2F211802oixbk4pppzogpbiy.jpg',
+        ],
+    },
+};
