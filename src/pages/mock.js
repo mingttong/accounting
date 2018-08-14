d09b248453c6fce7891a3215fae5fb27ce40e7bd
zhouwunan <zhouwunan@58ganji.com>
2018-08-14 10:24:09 +0800

    update

--- src/pages/mock.js
+++ src/pages/mock.js
@@ -2,67 +2,28 @@ module.exports = {
     recommand: [
         {
             name: 'Jay',
-            avator: 'http://pic7.58cdn.com.cn/anjuke_58/4b71f584c031a1267559ede388104bc5?w=62',
+            avator: 'https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1534175502731&di=2b2fd4b6300d9f000d6dccbe6d3ad8c5&imgtype=0&src=http%3A%2F%2Fatt.bbs.duowan.com%2Fforum%2F201407%2F26%2F211802oixbk4pppzogpbiy.jpg',
             tags: [
                 '技术好',
                 '人好',
             ],
             album: [
-                'http://pic7.58cdn.com.cn/anjuke_58/4b71f584c031a1267559ede388104bc5?w=62',
-                'http://pic7.58cdn.com.cn/anjuke_58/4b71f584c031a1267559ede388104bc5?w=62',
-                'http://pic7.58cdn.com.cn/anjuke_58/4b71f584c031a1267559ede388104bc5?w=62',
+                'http://img.zcool.cn/community/014dcc5652e60632f87512f66e74e8.JPG@1280w_1l_2o_100sh.jpg',
+                'https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1534175502731&di=2b2fd4b6300d9f000d6dccbe6d3ad8c5&imgtype=0&src=http%3A%2F%2Fatt.bbs.duowan.com%2Fforum%2F201407%2F26%2F211802oixbk4pppzogpbiy.jpg',
+                'http://img.zcool.cn/community/010c3c55e022ac32f875a132206cd5.jpg@1280w_1l_2o_100sh.jpg',
             ],
         },
         {
             name: 'Jay',
-            avator: 'http://pic7.58cdn.com.cn/anjuke_58/4b71f584c031a1267559ede388104bc5?w=62',
+            avator: 'https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1534220621464&di=03cde01d0a867d1289bf853f54fa5d74&imgtype=0&src=http%3A%2F%2Fimg1.ph.126.net%2FA-rRZ-Bj1NbTQexbgp6gew%3D%3D%2F6630587279119062250.jpg',
             tags: [
                 '技术好',
                 '人好',
             ],
             album: [
-                'http://pic7.58cdn.com.cn/anjuke_58/4b71f584c031a1267559ede388104bc5?w=62',
-                'http://pic7.58cdn.com.cn/anjuke_58/4b71f584c031a1267559ede388104bc5?w=62',
-                'http://pic7.58cdn.com.cn/anjuke_58/4b71f584c031a1267559ede388104bc5?w=62',
-            ],
-        },
-        {
-            name: 'Jay',
-            avator: 'http://pic7.58cdn.com.cn/anjuke_58/4b71f584c031a1267559ede388104bc5?w=62',
-            tags: [
-                '技术好',
-                '人好',
-            ],
-            album: [
-                'http://pic7.58cdn.com.cn/anjuke_58/4b71f584c031a1267559ede388104bc5?w=62',
-                'http://pic7.58cdn.com.cn/anjuke_58/4b71f584c031a1267559ede388104bc5?w=62',
-                'http://pic7.58cdn.com.cn/anjuke_58/4b71f584c031a1267559ede388104bc5?w=62',
-            ],
-        },
-        {
-            name: 'Jay',
-            avator: 'http://pic7.58cdn.com.cn/anjuke_58/4b71f584c031a1267559ede388104bc5?w=62',
-            tags: [
-                '技术好',
-                '人好',
-            ],
-            album: [
-                'http://pic7.58cdn.com.cn/anjuke_58/4b71f584c031a1267559ede388104bc5?w=62',
-                'http://pic7.58cdn.com.cn/anjuke_58/4b71f584c031a1267559ede388104bc5?w=62',
-                'http://pic7.58cdn.com.cn/anjuke_58/4b71f584c031a1267559ede388104bc5?w=62',
-            ],
-        },
-        {
-            name: 'Jay',
-            avator: 'http://pic7.58cdn.com.cn/anjuke_58/4b71f584c031a1267559ede388104bc5?w=62',
-            tags: [
-                '技术好',
-                '人好',
-            ],
-            album: [
-                'http://pic7.58cdn.com.cn/anjuke_58/4b71f584c031a1267559ede388104bc5?w=62',
-                'http://pic7.58cdn.com.cn/anjuke_58/4b71f584c031a1267559ede388104bc5?w=62',
-                'http://pic7.58cdn.com.cn/anjuke_58/4b71f584c031a1267559ede388104bc5?w=62',
+                'https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1534175502731&di=2b2fd4b6300d9f000d6dccbe6d3ad8c5&imgtype=0&src=http%3A%2F%2Fatt.bbs.duowan.com%2Fforum%2F201407%2F26%2F211802oixbk4pppzogpbiy.jpg',
+                'http://img.zcool.cn/community/010c3c55e022ac32f875a132206cd5.jpg@1280w_1l_2o_100sh.jpg',
+                'http://img.zcool.cn/community/014dcc5652e60632f87512f66e74e8.JPG@1280w_1l_2o_100sh.jpg',
             ],
         },
     ],
@@ -79,6 +40,33 @@ module.exports = {
         images: [
             'https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1534175502731&di=2b2fd4b6300d9f000d6dccbe6d3ad8c5&imgtype=0&src=http%3A%2F%2Fatt.bbs.duowan.com%2Fforum%2F201407%2F26%2F211802oixbk4pppzogpbiy.jpg',
             'https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1534175502731&di=2b2fd4b6300d9f000d6dccbe6d3ad8c5&imgtype=0&src=http%3A%2F%2Fatt.bbs.duowan.com%2Fforum%2F201407%2F26%2F211802oixbk4pppzogpbiy.jpg',
+        ],
+    },
+
+    index: {
+        list: [
+            {
+                avator: 'http://s1.sinaimg.cn/mw690/6794667agdd603e92fe90&690',
+                username: '九月的猫（用户）',
+                time: '2018-07-01',
+                position: '成都',
+                post: 'http://img.zcool.cn/community/014dcc5652e60632f87512f66e74e8.JPG@1280w_1l_2o_100sh.jpg',
+                title: '约摄影师',
+                tags: ['旅游', '网红景点'],
+                summary: '成都附近的，想拍出一组图四五六类型的照片，服装费和场地费我出',
+                type: 0,
+            },
+            {
+                avator: 'http://img.zcool.cn/community/010c3c55e022ac32f875a132206cd5.jpg@1280w_1l_2o_100sh.jpg',
+                username: '吉米（摄影师）',
+                time: '2018-06-28',
+                position: '成都',
+                post: 'https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1534175502731&di=2b2fd4b6300d9f000d6dccbe6d3ad8c5&imgtype=0&src=http%3A%2F%2Fatt.bbs.duowan.com%2Fforum%2F201407%2F26%2F211802oixbk4pppzogpbiy.jpg',
+                title: '约摄影师-风景画',
+                tags: ['旅游', '网红景点'],
+                summary: '北海 涠洲岛旅拍 8月4号~8月8号',
+                type: 1,
+            },
         ],
     },
 };
